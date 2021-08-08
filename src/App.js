1f86cf15b67589c84d6c444771f52cf290f5b504
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-08 22:22:17 +0530

    added routing and initial layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>Hiii Hacker! 2</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          {/* <Route exact path="/">
+          xxxxx
+        </Route> */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>This is 404 page.</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Hiii Homey.</div>;
+}
+
+export default Home;
