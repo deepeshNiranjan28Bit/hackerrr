5259522970c7be3edf8fd47bd771cf235e65bdbc
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-08 23:37:36 +0530

    added styled-components and clickable operations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { apiGet } from "../misc/config";
+import { SearchButtonWrapper, SearchInput } from "./Home.styled";
 
 function Home() {
-  return <div>Hiii Homey.</div>;
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search?query=${input}`).then((result) => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = useCallback(
+    (ev) => {
+      setInput(ev.target.value);
+    },
+    [setInput]
+  );
+
+  const onKeyDown = (ev) => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div>
+        <h1>HACKERRR</h1>
+        <h4>Wanna know about hacker! Come let me hack you.</h4>
+      </div>
+      <SearchInput
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+      </SearchButtonWrapper>
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home.styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const SearchInput = styled.input`
+  display: block;
+  font-family: "Roboto", sans-serif;
+  width: 200px;
+  margin: auto;
+  margin-bottom: 8px;
+  outline: none;
+  padding: 13px 15px;
+  border: 1px solid #dbdbdb;
+  box-shadow: 0px 0px 10px 0px rgba(219, 219, 219, 0.5);
+  font-size: 14px;
+  border-radius: 12px;
+  color: #8d8d8d;
+
+  &::placeholder {
+    font-weight: 300;
+    color: #8d8d8d;
+  }
+`;
+
+export const SearchButtonWrapper = styled.div`
+  text-align: center;
+  margin-bottom: 35px;
+
+  button {
+    color: #fff;
+    background-color: blue;
+    margin: auto;
+    padding: 10px 50px;
+    font-size: 15px;
+    border: none;
+    outline: none;
+    border-radius: 12px;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
